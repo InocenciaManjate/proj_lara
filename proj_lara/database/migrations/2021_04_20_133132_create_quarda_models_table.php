703b2d9216d00cc51a85e58d4b65ece915b5b69d
Valter Cinquenta <valthercinquenta@gmail.com>
2021-04-20 23:04:21 +0200

    Update 2021_04_20_133132_create_quarda_models_table.php

--- proj_lara/database/migrations/2021_04_20_133132_create_quarda_models_table.php
+++ proj_lara/database/migrations/2021_04_20_133132_create_quarda_models_table.php
@@ -17,6 +17,7 @@ class CreateQuardaModelsTable extends Migration
             $table->increments('id');
             $table->string('apelido');
             $table->string('nome');
+            $table->string('numero');
             $table->string('data');
             $table->string('cadeia');
             $table->timestamps();
