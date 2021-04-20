f951aec4daa5887747b3413e57c92d41260267bb
Inocencia Manjate <inocenciaarnaldo@gmail.com>
2021-04-20 15:04:17 +0300

    Update recluso.blade.php

--- proj_lara/resources/views/recluso.blade.php
+++ proj_lara/resources/views/recluso.blade.php
@@ -1,9 +1,21 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-6">
-			<form class="form-horizontal" method="POST" action="{{}}">
+			<form class="form-horizontal" method="POST" action="{{url('/insert')}}">
+        {{csrf_field()}}
   <fieldset>
     <legend>Recluso</legend>
+
+    @if(count($errors) > 0)
+     @foreach($errors->all() as $error)
+
+      <div class="alert alert-danger">
+        {{$error}}
+      </div>
+
+     @endforeach
+     @endif
+
     <div class="form-group">
       <label for="inputEmail" class="col-lg-2 control-label">Apelido</label>
       <div class="col-lg-10">
@@ -52,9 +64,10 @@
     
     <div class="form-group">
       <div class="col-lg-10 col-lg-offset-2">
-        <button type="reset" class="btn btn-default">Cancel</button>
         <button type="submit" class="btn btn-primary">Submit</button>
       </div>
+       
+    <a href="{{url('/')}}" class="btn btn-primary">Voltar</a>
     </div>
   </fieldset>
 </form>
