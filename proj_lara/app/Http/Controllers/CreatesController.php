c315622a56042a14f63bbc0c78303a4c7a17f90b
Inocencia Manjate <inocenciaarnaldo@gmail.com>
2021-04-20 15:04:00 +0300

    Update CreatesController.php

--- proj_lara/app/Http/Controllers/CreatesController.php
+++ proj_lara/app/Http/Controllers/CreatesController.php
@@ -10,6 +10,28 @@ class CreatesController extends Controller
     public function home(){
     	$articles = Article::all();
     	return view('home', ['articles'=> $articles]);
+}
 
-    }
+
+    public function add(Request $request){
+    	$this->validate($request,[
+         'apelido'=> 'required',
+         'nome'=> 'required',
+         'tipoCrime'=> 'required',
+         'data'=> 'required',
+         'numero'=> 'required',
+         'cadeia'=> 'required'
+
+    	]);
+
+    	$articles = new Article;
+    	$articles ->apelido = $request-> input('apelido');
+    	$articles ->nome = $request-> input('nome');
+    	$articles ->tipoCrime = $request-> input('tipoCrime');
+    	$articles ->data = $request-> input('data');
+    	$articles ->numero = $request-> input('numero');
+    	$articles ->cadeia = $request-> input('cadeia');
+    	$articles ->save();
+    	return redirect('/')->with('info','Salvo com sucesso');
+    }	
 }
